9841da8350a351dce331074b3fcb13a088ad4bd5
Edward Gao <edsperanto@users.noreply.github.com>
2017-10-04 22:54:00 -0700

    Only run commands when deployed, and make messages red

--- install.sh
+++ install.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-echo "Installing HomeBrew for MacOS..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+DEPLOY=false
+RED='\033[0;31m'
+NC='\033[0m'
 
-echo "Installing zsh, tmux, nodejs, and npm..."
-brew install zsh tmux nodejs npm
+echo "${RED}Installing HomeBrew for MacOS...${NC}"
+if $DEPLOY ; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-echo "Setting up NodeJS environment..."
-sudo npm i -g n
-sudo n lts
-sudo npm i -g npm
+echo "${RED}Installing zsh, tmux, nodejs, and npm...${NC}"
+if $DEPLOY ; then
+  brew install zsh tmux nodejs npm
+fi
 
-echo "Setting up Github environment..."
-git config --global user.name "Edward Gao"
-git config --global user.email "edsperanto@users.noreply.github.com"
-git config --global push.default simple
-#ssh-keygen -t rsa -b 4096 -C "edsperanto@users.noreply.github.com"
-#eval "$(ssh-agent -s)"
-#ssh-add ~/.ssh/id_rsa
-echo "Please copy ~/.ssh/id_rsa.pub to Gibhub SSH keys"
+echo "${RED}Setting up NodeJS environment...${NC}"
+if $DEPLOY ; then
+  sudo npm i -g n
+  sudo n lts
+  sudo npm i -g npm
+fi
+
+echo "${RED}Setting up Github environment...${NC}"
+if $DEPLOY ; then
+  git config --global user.name "Edward Gao"
+  git config --global user.email "edsperanto@users.noreply.github.com"
+  git config --global push.default simple
+  ssh-keygen -t rsa -b 4096 -C "edsperanto@users.noreply.github.com"
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+fi
+echo "${RED}Please copy ~/.ssh/id_rsa.pub to Gibhub SSH keys${NC}"
