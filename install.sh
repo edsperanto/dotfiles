a2d7617847a1862ba8e27a56a2b0b8ee8d481cab
Edward Gao <edsperanto@users.noreply.github.com>
2017-11-09 20:19:26 -0800

    added pauses for user confirmation

--- install.sh
+++ install.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
+# Constants
 DEPLOY=false
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo "${RED}Installing HomeBrew for MacOS...${NC}"
+# Functions
+function warn() {
+  echo "${RED}$*${NC}"
+}
+function fin() {
+  echo "${GREEN}$*${NC}"
+  read -p "Press [ENTER] to continue."
+}
+
+warn "Installing HomeBrew for MacOS..."
 if $DEPLOY ; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+fin "HomeBrew installed."
 
-echo "${RED}Installing zsh, tmux, nodejs, and npm...${NC}"
+warn "Installing zsh, tmux, nodejs, and npm..."
 if $DEPLOY ; then
   brew install zsh tmux nodejs npm
 fi
+fin "zsh, tmux, node, and npm installed."
 
-echo "${RED}Setting up NodeJS environment...${NC}"
+warn "Setting up NodeJS environment..."
 if $DEPLOY ; then
   sudo npm i -g n
   sudo n lts
   sudo npm i -g npm
 fi
+fin "Node environment set up complete."
 
 echo "${RED}Setting up Github environment...${NC}"
 if $DEPLOY ; then
-  git config --global user.name "Edward Gao"
-  git config --global user.email "edsperanto@users.noreply.github.com"
-  git config --global push.default simple
+  sudo git config --global user.name "Edward Gao"
+  sudo git config --global user.email "edsperanto@users.noreply.github.com"
+  sudo git config --global push.default simple
   ssh-keygen -t rsa -b 4096 -C "edsperanto@users.noreply.github.com"
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 fi
-echo "${RED}Please copy ~/.ssh/id_rsa.pub to Gibhub SSH keys${NC}"
+warn "----------------- Start of SSH public key -----------------"
+cat ~/.ssh/id_rsa.pub
+warn "------------------ End of SSH public key ------------------"
+fin "\nPlease copy the above public key to to Gibhub"
